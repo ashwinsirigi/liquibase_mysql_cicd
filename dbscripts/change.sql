ade5de1c31760891182a78038bc1b789f7f6962f
ashwinsirigi <ashwinsirigi@gmail.com>
2024-07-05 22:29:46 +0400

    New Table usercontact Create

--- dbscripts/change.sql
+++ dbscripts/change.sql
@@ -6,5 +6,5 @@ ALTER TABLE users ADD COLUMN age INT,algorithm=inplace,lock=none;
 
 --changeset usercontact-new-tab:2 labels:usercontact-new-tab-label context:usercontact-new-tab-context
 --comment: creating new table
-create table usercontact (id int primary key , phone varchar(15),addr varchar(100));  
+create table usercontact (id int primary key , phone varchar(15),addr varchar(100));    
 
